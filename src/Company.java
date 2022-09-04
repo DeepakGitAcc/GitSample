cc13edf5e3b1a92587f443f99ea48d8960e8b016
DeepakGitAcc <deepak97.a@gmail.com>
2022-09-04 20:24:17 +0530

    Company

---
+++ src/Company.java
@@ -0,0 +1,22 @@
+
+//Child class
+public class Company {
+
+	public void cmpId() {
+		System.out.println("company id is 112233");
+
+	}
+	public void cmpNa() {
+		System.out.println("company name is CTS");
+
+	}
+	public static void main(String[] args) {
+		Company c = new Company();
+				c.cmpId();
+				c.cmpNa();
+				
+	}
+	
+}
+
+
